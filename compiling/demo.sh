c3a1285f6b0bd0052fd5e1eeb3febf9b67ada88e
Prince Nwaonicha <nwaon005@bistromath.morris.umn.edu>
2018-09-04 14:42:30 -0500

    Updated

---
+++ compiling/demo.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+echo "The first argument was $1"
+echo "The second argument was $2"
--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,3 +1,10 @@
-./extract_and_compile.sh
+#/bin/bash
+mkdir $2
 
+tar zxf NthPrime.tgz -C $2/
 
+cd $2/NthPrime
+
+gcc -o new main.c nth_prime.c
+
+./new $1
